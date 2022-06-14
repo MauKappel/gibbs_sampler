99b65e0082baa40d3abf5b3502bc779d533bfc2c
Ericbf10 <eric.bautista01@estudiant.upf.edu>
2022-06-14 16:46:44 +0200

    Cross-validation code v2

--- scripts/04_cross_validation.py
+++ scripts/04_cross_validation.py
@@ -5,6 +5,7 @@
 
 import os
 import random
+import subprocess
 import numpy as np
 import math
 from argparse import ArgumentParser
@@ -12,9 +13,13 @@ from argparse import ArgumentParser
 ## PARSER
 parser = ArgumentParser(description="Cross-validation")
 parser.add_argument("-s", action="store", dest="seed", type=int, default=1, help="Random number seed (default: 1)")
+parser.add_argument("-a", action="store", dest="allele", type=str, help="Allele (shared string)")
+parser.add_argument("-k", action="store", dest="k", type=int, default=5, help="Cross-validation fold (default: 5)")
 
 args = parser.parse_args()
 seed = args.seed
+allele = args.allele
+k = args.k
 
 ## FUNCTIONS
 
@@ -22,12 +27,21 @@ seed = args.seed
 
 # Data path
 script_path = os.getcwd()
-data_dir = os.path.join(script_path, '../non-binders/')
+data_dir = os.path.join(script_path, '../data/non-binders/')
 
 # Define random seed
 random.seed(seed)
 
-# Retrieve evaluation file
-
-
-# Define training files and test file
+# Retrieve evaluation file and run k CV fold Gibbs Sampler
+job = subprocess.run(['ls ' + data_dir], shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+infile_list = job.stdout.split('\n')
+infile_list.pop()
+for element in infile_list:
+    if allele in element:
+        allele_dir = data_dir + element + "/c00"
+        evaluation_file = allele_dir + "0"
+        for i in range(1, 5):
+            training_index = [1, 2, 3, 4]
+            training_index.remove(i)
+            test_file = allele_dir + str(i)
+            training_file =
