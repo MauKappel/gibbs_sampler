6c13eae0631deef26d756ffa5b38e86b2452708f
jfunk-biodesign <funk.jonathan21@gmail.com>
2022-06-15 13:10:15 +0200

    constructed similarity matrix

--- scripts/02_align_O2_all_vs_all.py
+++ scripts/02_align_O2_all_vs_all.py
@@ -1,30 +1,47 @@
 import numpy as np
 import os
 from argparse import ArgumentParser
+import sys
+import matplotlib.pylab as plt
 
-parser = ArgumentParser(description="Alignment_O2")
-
-parser.add_argument("-q", action="store", dest="query_file", help="File with query sequence")
-parser.add_argument("-db", action="store", dest="db_file", help="File with database sequence")
+parser = ArgumentParser(description="Alignment_O2, if only q or db is provided, sequences will be align within file")
+parser.add_argument("-q", action="store", dest="query_file", default=None, help="File with query sequence")
+parser.add_argument("-db", action="store", dest="db_file", default=None,help="File with database sequence")
 parser.add_argument("-go", action="store", dest="gap_open", type=float, default=-11.0, help="Value of gap open (-11.0)")
+parser.add_argument("-debug", action="store", dest="DEBUG", type=bool, default=False, help="only align a small number of sequences")
 parser.add_argument("-ge", action="store", dest="gap_extension", type=float, default=-1.0,
                     help="Value of gap extension (-1.0)")
+parser.add_argument("-t", action="store", dest="thresh", type=float, default=0.8, help="Value of similarity theshold (0.8)")
 
 args = parser.parse_args()
-
 gap_open = args.gap_open
 gap_extension = args.gap_extension
-query_file = args.query_file
-database_file = args.db_file
+
+## If only one file is provided allign all vs all within the file, else align 2 files
+if args.db_file == None and args.query_file == None:
+    print('provide alignment file')
+    sys.exit()
+
+elif args.query_file == None:
+    query_file = args.db_file
+    database_file = args.db_file
+
+elif args.db_file == None:
+    database_file = args.query_file
+    query_file = args.query_file
+
+else:
+    query_file = args.query_file
+    database_file = args.db_file
 
 script_path = os.getcwd()
-data_dir = os.path.join(script_path, '../non-binders/')
+data_dir = os.path.join(script_path, '../data/')
 
+### Amino Acid codes
 alphabet_file = data_dir + "Matrices/alphabet"
 alphabet = np.loadtxt(alphabet_file, dtype=str)
 
-# ### Blosum Matrix
-
+### Blosum Matrix
 blosum_file = data_dir + "Matrices/BLOSUM50"
 _blosum50 = np.loadtxt(blosum_file, dtype=float).reshape((24, -1)).T
 
@@ -38,25 +55,21 @@ for i, letter_1 in enumerate(alphabet):
         blosum50[letter_1][letter_2] = _blosum50[i, j]
 
 
-# ## Alignment Matrix
-#
+## Alignment Matrix
 # This functions returns, apart from the final Alignment Matrix, all the intermedite Matrices (for plotting purposes).
-#
-#
-
 def smith_waterman_alignment(query="VLLP", database="VLILP", scoring_scheme={}, gap_open=-5, gap_extension=-1):
     # Matrix imensions
     M = len(query)
     N = len(database)
 
     # D matrix change to float
-    D_matrix = np.zeros((M + 1, N + 1), np.int)
+    D_matrix = np.zeros((M + 1, N + 1), np.int32)
 
     # P matrix
-    P_matrix = np.zeros((M + 1, N + 1), np.int)
+    P_matrix = np.zeros((M + 1, N + 1), np.int32)
 
     # Q matrix
-    Q_matrix = np.zeros((M + 1, N + 1), np.int)
+    Q_matrix = np.zeros((M + 1, N + 1), np.int32)
 
     # E matrix
     E_matrix = np.zeros((M + 1, N + 1), dtype=object)
@@ -129,7 +142,6 @@ def smith_waterman_alignment(query="VLLP", database="VLILP", scoring_scheme={}, 
 
 
 # ## Alignment Matrix Traceback
-
 def smith_waterman_traceback(E_matrix, D_matrix, i_max, j_max, query="VLLP", database="VLILP", gap_open=-5,
                              gap_extension=-1):
     M = len(query)
@@ -204,12 +216,17 @@ def smith_waterman_traceback(E_matrix, D_matrix, i_max, j_max, query="VLLP", dat
     return aligned_query, aligned_database, matches
 
 
-# ### Align query against database
+### Align query against database#
 
-# In[11]:
+if args.DEBUG == False:
+    print(f'aligning {query_file} vs {database_file}')
+    query_list = np.loadtxt(query_file, dtype=str, delimiter=' ').reshape(-1, 2)
+    database_list = np.loadtxt(database_file, dtype=str).reshape(-1, 2)
+else:
+    print('debug_mode')
+    query_list = np.loadtxt(query_file, dtype=str, delimiter=' ').reshape(-1, 2)[0:100]
+    database_list = np.loadtxt(database_file, dtype=str).reshape(-1, 2)[0:100]
 
-query_list = np.loadtxt(query_file, dtype=str).reshape(-1, 2)
-database_list = np.loadtxt(database_file, dtype=str).reshape(-1, 2)
 
 from time import time
 
@@ -217,15 +234,13 @@ scoring_scheme = blosum50
 
 # this returns current timestamp in seconds
 t0 = time()
+all_matches = []
+match_matrix = np.zeros((len(query_list), len(database_list)))
+for m, query in enumerate(query_list):
+    query_sequence = query[0]
 
-for query in query_list:
-
-    query_protein = query[0]
-    query_sequence = query[1]
-
-    for database in database_list:
-        database_protein = database[0]
-        database_sequence = database[1]
+    for n, database in enumerate(database_list):
+        database_sequence = database[0]
 
         P_matrix, Q_matrix, D_matrix, E_matrix, i_max, j_max, max_score = smith_waterman_alignment(query_sequence,
                                                                                                    database_sequence,
@@ -235,13 +250,29 @@ for query in query_list:
         aligned_query, aligned_database, matches = smith_waterman_traceback(E_matrix, D_matrix, i_max, j_max,
                                                                             query_sequence, database_sequence, gap_open,
                                                                             gap_extension)
-
-        print("ALN", query_protein, len(query_sequence), database_protein, len(database_sequence), len(aligned_query),
-              matches, max_score)
-        print("QAL", i_max, ''.join(aligned_query))
-        print("DAL", j_max, ''.join(aligned_database))
-        print("")
+        '''
+        TODO:
+        - change the code so it only accepts one file
+        - calculate the sum of the rows, remove the rows and corresponding column which have the highest count
+        
+        NOTE: 
+        - Theshold greater than 80% matches will be a 1, lower than 80% matches will be 0
+        '''
+        #print("ALN", query_sequence, len(query_sequence), database_sequence, len(database_sequence), len(aligned_query),
+              #matches, max_score)
+        #print("QAL", i_max, ''.join(aligned_query))
+        #print("DAL", j_max, ''.join(aligned_database))
+        if query_sequence != database_sequence:
+            if matches > args.thresh * len(query_sequence):
+                all_matches.append(matches)
+                match_matrix[m][n] = 1
+            else:
+                all_matches.append(matches)
+                match_matrix[m][n] = 0
 
 t1 = time()
-
 print("Time (m):,", (t1 - t0) / 60)
+print(match_matrix)
+print(np.apply_along_axis(sum, axis=1, arr=match_matrix))
+plt.hist(all_matches)
+plt.savefig('../results/02_alignement_matches.png')
--- scripts/03_hobohm2.py
+++ scripts/03_hobohm2.py
@@ -4,11 +4,11 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser(description="Hobohm2")
-parser.add_argument("-f", action="store", dest="ALIGNEMENT_FILE", type=str, default="DRB1_0101/all", help="Alignement file, place in raw_data/Hobholm")
+parser.add_argument("-f", action="store", dest="ALIGNEMENT_FILE", type=str, default="non-binders/DRB1_0101/all", help="Alignement file, place in raw_data/Hobholm")
 args = parser.parse_args()
 
 script_path = os.getcwd()
-data_dir = os.path.join(script_path, '../non-binders/')
+data_dir = os.path.join(script_path, '../data/')
 
 # reading file
 alignment_file_part = os.path.join(data_dir, f"{args.ALIGNEMENT_FILE}")
