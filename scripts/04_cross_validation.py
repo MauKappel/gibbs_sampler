4c621144fc1b8fa7eed3084d6937fb2b7db76799
Ericbf10 <eric.bautista01@estudiant.upf.edu>
2022-06-16 16:39:13 +0200

    Cv code implement hobohm1

--- scripts/04_cross_validation.py
+++ scripts/04_cross_validation.py
@@ -7,7 +7,6 @@ import os
 import random
 import subprocess
 from joblib import Parallel, delayed
-from time import time
 from argparse import ArgumentParser
 
 ## PARSER
@@ -15,7 +14,7 @@ parser = ArgumentParser(description="Cross-validation")
 parser.add_argument("-s", action="store", dest="seed", type=int, default=1, help="Random number seed (default: 1)")
 parser.add_argument("-a", action="store", dest="allele", type=str, help="Allele (shared string)")
 parser.add_argument("-k", action="store", dest="k", type=int, default=5, help="Cross-validation fold (default: 5)")
-parser.add_argument("-m", action="store", dest="method", type=str, default="simple_gibbs", help="Method used in the algorithm (options: simple_gibbs(default), hobohm2, seq_weight)")
+parser.add_argument("-m", action="store", dest="method", type=str, default="simple_gibbs", help="Method used in the algorithm (options: simple_gibbs(default), hobohm1, hobohm2, seq_weight)")
 
 args = parser.parse_args()
 seed = args.seed
@@ -58,6 +57,13 @@ for full_allele in infile_list:
                                   " -debug True -t 0.2"], shell=True, stdout=subprocess.PIPE, universal_newlines=True)
             allele_dir = hobohm_dir + "/c00"
             evaluation_file = allele_dir + "0"
+        elif method == "hobohm1":
+            hobohm_dir = os.path.join(script_path, "../data/hobohm1/" + full_allele)
+            #os.makedirs(hobohm_dir)
+            job = subprocess.run(["python3 02_hobohm1.py -f " + data_dir + full_allele + "/all" + " -d " + hobohm_dir],
+                                 shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+            allele_dir = hobohm_dir + "/c00"
+            evaluation_file = allele_dir + "0"
         else:
             allele_dir = data_dir + full_allele + "/c00"
             evaluation_file = allele_dir + "0"
@@ -81,6 +87,8 @@ for full_allele in infile_list:
 # Parallelize Gibbs Sampler call
 result = Parallel(n_jobs=8)(delayed(unix_call)(job) for job in job_list)
 
+# INCLUDE SLEEP STEP?
+
 # Retrieve mean of all 4 evaluations
 eval_job_list = []
 for allele in allele_list:
@@ -89,7 +97,7 @@ for allele in allele_list:
     mat_file = allele_dir + "mat_file_"
     output_file = allele_dir + "final_out"
     output_plot = allele_dir + "plot"
-    eval_job_list.append("python3 05_evaluation_v2.py -k " + k + " -e " +  eval_file + " -mat " + mat_file + " -of " + output_file + " -op " + output_plot)
+    eval_job_list.append("python3 05_evaluation_v2.py -k " + k + " -e " + eval_file + " -mat " + mat_file + " -of " + output_file + " -op " + output_plot)
 
 # Parallelize Evaluation call
 result = Parallel(n_jobs=8)(delayed(unix_call)(job) for job in eval_job_list)
